021b92da54cd5ab25fdaf810137f4165438bbe14
Stephen Malone <mail@gesedels.com>
2025-08-09 05:57:21 +1000

    Added /icon handler.

--- stnts.go
+++ stnts.go
@@ -12,6 +12,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync"
+	"time"
 )
 
 ///////////////////////////////////////////////////////////////////////////////////////
@@ -21,28 +23,45 @@ import (
 // 1.1: command-line flags
 ///////////////////////////
 
-// MainFlags is the default command-line flag parser.
-var MainFlags = flag.NewFlagSet("stnts", flag.ExitOnError)
+// Flags is the default command-line flag parser.
+var Flags = flag.NewFlagSet("stnts", flag.ExitOnError)
 
 // Defined command-line flags.
 var (
-	FlagAddr  = MainFlags.String("addr", "localhost:8000", "host:port address")
-	FlagCache = MainFlags.String("cache", "./cache", "cache folder")
-	FlagConf  = MainFlags.String("conf", "", "configuration file")
-	FlagDebug = MainFlags.Bool("debug", false, "enable debug mode")
+	FlagAddr  = Flags.String("addr", "localhost:8000", "host:port address")
+	FlagConf  = Flags.String("conf", "", "configuration file")
+	FlagDebug = Flags.Bool("debug", false, "enable debug mode")
 )
 
 // 1.2: main control variables
 ///////////////////////////////
 
-// MainLogs is the default system logger.
-var MainLogs *log.Logger
+// Log is the global system logger.
+var Log *log.Logger
 
-// MainMux is the default server handler mux.
-var MainMux *http.ServeMux
+// Mux is the global server handler mux.
+var Mux *http.ServeMux
 
-// MainServer is the default system server.
-var MainServer *http.Server
+// Server is the global system server.
+var Server *http.Server
+
+// 1.3: main storage variables
+///////////////////////////////
+
+// IconCache is the global icon file cache map.
+var IconCache map[string][]byte
+
+// IconCacheLock is the global mutex for IconCache.
+var IconCacheLock *sync.Mutex
+
+// IconTypes is a list of valid icon filenames.
+var IconTypes = []string{
+	"apple-touch-icon.png",
+	"apple-touch-icon-180x180.png",
+	"favicon.png",
+	"favicon.svg",
+	"favicon.ico",
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////
 //                       part two · file and download functions                      //
@@ -50,7 +69,8 @@ var MainServer *http.Server
 
 // DownloadURL returns a URL's contents as a byteslice.
 func DownloadURL(addr string) ([]byte, error) {
-	resp, err := http.Get(addr)
+	client := &http.Client{Timeout: time.Second}
+	resp, err := client.Get(addr)
 	switch {
 	case err != nil:
 		return nil, fmt.Errorf("cannot download %q - %w", addr, err)
@@ -58,20 +78,21 @@ func DownloadURL(addr string) ([]byte, error) {
 		return nil, fmt.Errorf("cannot download %q - status %d", addr, resp.StatusCode)
 	}
 
+	defer resp.Body.Close()
 	return io.ReadAll(resp.Body)
 }
 
 // DownloadURLs returns the first valid URL's contents as a byteslice.
-func DownloadURLs(root string, names ...string) ([]byte, error) {
+func DownloadURLs(host string, names ...string) ([]byte, error) {
 	for _, name := range names {
-		addr := fmt.Sprintf("https://%s/%s", root, name)
+		addr := fmt.Sprintf("https://%s/%s", host, name)
 		bytes, err := DownloadURL(addr)
 		if err == nil {
 			return bytes, nil
 		}
 	}
 
-	return nil, fmt.Errorf("cannot download from %q - no accessible URLs", root)
+	return nil, fmt.Errorf("cannot download from %q - no accessible URLs", host)
 }
 
 // ReadJSON unmarshals a JSON file into an object.
@@ -85,32 +106,80 @@ func ReadJSON(orig string, data any) error {
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////
+//                         part ??? · http response functions                        //
+///////////////////////////////////////////////////////////////////////////////////////
+
+// WriteError writes a formatted text/plain error message to a ResponseWriter.
+func WriteError(w http.ResponseWriter, code int, text string, elems ...any) {
+	text = fmt.Sprintf("error %d: %s", code, text)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(code)
+	fmt.Fprintf(w, text, elems...)
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+//                            part ??? · handler functions                           //
+///////////////////////////////////////////////////////////////////////////////////////
+
+// GetIcon returns a new or cached remote icon file.
+func GetIcon(w http.ResponseWriter, r *http.Request) {
+	host := r.PathValue("host")
+	if _, ok := IconCache[host]; !ok {
+		bytes, err := DownloadURLs(host, IconTypes...)
+		if err != nil {
+			WriteError(w, http.StatusNotFound, "%q icon not found", host)
+			return
+		}
+
+		IconCacheLock.Lock()
+		IconCache[host] = bytes
+		IconCacheLock.Unlock()
+	}
+
+	mime := http.DetectContentType(IconCache[host])
+	w.Header().Set("Content-Type", mime)
+	w.WriteHeader(http.StatusOK)
+	w.Write(IconCache[host])
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+//                          part ??? · middleware functions                          //
+///////////////////////////////////////////////////////////////////////////////////////
+
+// LoggingWare wraps a HandlerFunc in middleware to log every incoming request.
+func LoggingWare(hfun http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL.Path)
+		hfun(w, r)
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
 //                             part ??? · main functions                             //
 ///////////////////////////////////////////////////////////////////////////////////////
 
 // init initialises the stnts program before start.
 func init() {
 	// Parse command-line flags.
-	MainFlags.Parse(os.Args[1:])
+	Flags.Parse(os.Args[1:])
 
-	// Initialise MainLogs and print configuration.
-	MainLogs = log.New(os.Stdout, "", log.LstdFlags)
-	MainLogs.Printf("-addr=%q", *FlagAddr)
-	MainLogs.Printf("-cache=%q", *FlagCache)
-	MainLogs.Printf("-conf=%q", *FlagConf)
-	MainLogs.Printf("-debug is %t", *FlagDebug)
+	// Initialise control variables.
+	Log = log.New(os.Stdout, "", log.LstdFlags)
+	Mux = http.NewServeMux()
+	Server = &http.Server{Addr: *FlagAddr, Handler: Mux}
 
-	// Initialise MainMux.
-	MainMux = http.NewServeMux()
+	// Initialise content variables.
+	IconCache = make(map[string][]byte)
+	IconCacheLock = new(sync.Mutex)
 
-	// Initialise MainServer.
-	MainServer = &http.Server{Addr: *FlagAddr, Handler: MainMux}
+	// Register handler routes.
+	Mux.HandleFunc("GET /icon/{host...}", LoggingWare(GetIcon))
 }
 
 // main runs the main stnts program.
 func main() {
-	MainLogs.Printf("starting server on %q", *FlagAddr)
-	if err := MainServer.ListenAndServe(); err != nil {
-		MainLogs.Fatal(err)
+	Log.Printf("starting server on %q", *FlagAddr)
+	if err := Server.ListenAndServe(); err != nil {
+		Log.Fatal(err)
 	}
 }
